18bead10d7c09bd09067ea83bda7b0d90f9de9af
licinha-23 <alicegomeseberhardt@gmail.com>
2023-09-06 11:22:10 -0300

    commit 06.09

---
+++ Calculos/app/src/main/java/com/example/calculos/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.calculos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSoma = findViewById(R.id.btnSoma);
+        btnSoma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tela2 = new Intent(getApplicationContext(), Tela2.class);
+                startActivity(tela2);
+            }
+        });
+    }
+}
